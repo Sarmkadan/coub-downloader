347d47ebb865c28e572dbe581c3d9e881b85a740
Vladyslav Zaiets <rutova2@gmail.com>
2023-05-19 15:36:25 +0300

    .

--- tests/coub-downloader.Tests/DateTimeExtensionsTestsExtensions.cs
+++ tests/coub-downloader.Tests/DateTimeExtensionsTestsExtensions.cs
@@ -2,77 +2,44 @@ using System;
 
 namespace CoubDownloader.Tests
 {
+    /// <summary>
+    /// Extension methods for DateTime used in tests to provide consistent test data and assertions.
+    /// </summary>
     public static class DateTimeExtensionsTestsExtensions
     {
         /// <summary>
-        /// Converts a DateTime to a friendly relative time string (e.g., "2 minutes ago", "in 3 hours").
-        /// </summary>
-        public static string ToFriendlyRelativeTime(this DateTime dateTime)
-        {
-            var now = DateTime.Now;
-            var timeSpan = now - dateTime;
-
-            if (timeSpan.TotalSeconds < 60)
-            {
-                return "just now";
-            }
-
-            if (timeSpan.TotalMinutes < 60)
-            {
-                var minutes = (int)timeSpan.TotalMinutes;
-                return $"{minutes} minute{(minutes == 1 ? "" : "s")} ago";
-            }
-
-            if (timeSpan.TotalHours < 24)
-            {
-                var hours = (int)timeSpan.TotalHours;
-                return $"{hours} hour{(hours == 1 ? "" : "s")} ago";
-            }
-
-            if (timeSpan.TotalDays < 7)
-            {
-                var days = (int)timeSpan.TotalDays;
-                return $"{days} day{(days == 1 ? "" : "s")} ago";
-            }
-
-            if (timeSpan.TotalDays < 30)
-            {
-                var weeks = (int)(timeSpan.TotalDays / 7);
-                return $"{weeks} week{(weeks == 1 ? "" : "s")} ago";
-            }
-
-            if (timeSpan.TotalDays < 365)
-            {
-                var months = (int)(timeSpan.TotalDays / 30);
-                return $"{months} month{(months == 1 ? "" : "s")} ago";
-            }
-
-            var years = (int)(timeSpan.TotalDays / 365);
-            return $"{years} year{(years == 1 ? "" : "s")} ago";
-        }
-
-        /// <summary>
         /// Checks if a DateTime is within a specified range (inclusive).
         /// </summary>
+        /// <param name="dateTime">The DateTime to check.</param>
+        /// <param name="start">The start of the range (inclusive).</param>
+        /// <param name="end">The end of the range (inclusive).</param>
+        /// <returns>True if the DateTime is within the range; otherwise, false.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="start"/> is after <paramref name="end"/>.</exception>
         public static bool IsWithinRange(this DateTime dateTime, DateTime start, DateTime end)
         {
+            if (start > end)
+            {
+                throw new ArgumentOutOfRangeException(nameof(start), "Start date cannot be after end date.");
+            }
+
             return dateTime >= start && dateTime <= end;
         }
 
         /// <summary>
-        /// Returns the DateTime representing the start of the next day (midnight of the following day).
+        /// Converts a DateTime to Unix timestamp (seconds since 1970-01-01 UTC).
         /// </summary>
-        public static DateTime NextDay(this DateTime dateTime)
-        {
-            return dateTime.Date.AddDays(1);
-        }
-
-        /// <summary>
-        /// Converts a DateTime to Unix timestamp (seconds since 1970-01-01).
-        /// </summary>
+        /// <param name="dateTime">The DateTime to convert.</param>
+        /// <returns>The Unix timestamp in seconds.</returns>
+        /// <exception cref="OverflowException">Thrown when the DateTime represents a date before Unix epoch (1970-01-01).</exception>
         public static long ToUnixTimestamp(this DateTime dateTime)
         {
-            return (long)(dateTime - DateTime.UnixEpoch).TotalSeconds;
+            var result = (long)(dateTime.ToUniversalTime() - DateTime.UnixEpoch).TotalSeconds;
+            if (result < 0)
+            {
+                throw new OverflowException("DateTime represents a date before Unix epoch (1970-01-01).");
+            }
+
+            return result;
         }
     }
 }

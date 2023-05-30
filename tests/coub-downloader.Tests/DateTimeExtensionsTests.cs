e75020c70d023df1acf4a44d5e9e59aa02d9a438
Vladyslav Zaiets <rutova2@gmail.com>
2023-05-30 14:56:49 +0300

    small DateTimeExtensions helpers

--- tests/coub-downloader.Tests/DateTimeExtensionsTests.cs
+++ tests/coub-downloader.Tests/DateTimeExtensionsTests.cs
@@ -8,10 +8,16 @@ using CoubDownloader.Infrastructure.Utilities;
 using FluentAssertions;
 using Xunit;
 
-namespace CoubDownloader.Tests;
-
+/// <summary>
+/// Tests for the DateTimeExtensions class.
+/// </summary>
 public class DateTimeExtensionsTests
 {
+    /// <summary>
+    /// Tests the GetRelativeTime method with various time spans.
+    /// </summary>
+    /// <param name="secondsAgo">The number of seconds ago.</param>
+    /// <param name="expected">The expected relative time string.</param>
     [Theory]
     [InlineData(10, "just now")]
     [InlineData(65, "1m ago")]
@@ -24,6 +30,9 @@ public class DateTimeExtensionsTests
         result.Should().Be(expected);
     }
 
+    /// <summary>
+    /// Tests the FormatDuration method with a valid time span.
+    /// </summary>
     [Fact]
     public void FormatDuration_ValidTimeSpan_ReturnsFormattedString()
     {
@@ -32,6 +41,9 @@ public class DateTimeExtensionsTests
         result.Should().Be("01:02:03");
     }
 
+    /// <summary>
+    /// Tests the IsWithinRange method with a date in the specified range.
+    /// </summary>
     [Fact]
     public void IsWithinRange_DateInRange_ReturnsTrue()
     {
@@ -41,6 +53,9 @@ public class DateTimeExtensionsTests
         dateTime.IsWithinRange(start, end).Should().BeTrue();
     }
 
+    /// <summary>
+    /// Tests the StartOfDay method with a date and time.
+    /// </summary>
     [Fact]
     public void StartOfDay_ReturnsCorrectDatePart()
     {
@@ -49,6 +64,9 @@ public class DateTimeExtensionsTests
         dateTime.StartOfDay().Should().Be(expected);
     }
 
+    /// <summary>
+    /// Tests the StartOfWeek method with a date and time, specifying Monday as the start of the week.
+    /// </summary>
     [Fact]
     public void StartOfWeek_MondayStart_ReturnsCorrectDate()
     {
@@ -57,6 +75,9 @@ public class DateTimeExtensionsTests
         dateTime.StartOfWeek(DayOfWeek.Monday).Should().Be(expected);
     }
 
+    /// <summary>
+    /// Tests the ToUnixTimestamp method with a date and time, and then converts it back to a date and time using the FromUnixTimestamp method.
+    /// </summary>
     [Fact]
     public void ToUnixTimestamp_FromUnixTimestamp_RoundtripsCorrectly()
     {

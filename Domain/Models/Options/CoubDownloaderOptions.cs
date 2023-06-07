ebd7f3255698ffbbe0e510c68672ec5aa8762b89
Vladyslav Zaiets <rutova2@gmail.com>
2023-06-07 09:13:14 +0300

    Reshuffle CoubDownloaderOptions a bit

--- Domain/Models/Options/CoubDownloaderOptions.cs
+++ Domain/Models/Options/CoubDownloaderOptions.cs
@@ -2,24 +2,56 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CoubDownloader.Domain.Models.Options;
 
+/// <summary>
+/// Configuration options for download operations including output paths, retry behavior, and caching settings.
+/// </summary>
 public class DownloadOptions
 {
     [Required]
+    /// <summary>
+    /// Gets or sets the base directory where downloaded coubs will be saved.
+    /// Defaults to "./downloads".
+    /// </summary>
     public string OutputPath { get; set; } = "./downloads";
     
+    /// <summary>
+    /// Gets or sets the directory where downloaded coubs will be cached for reuse.
+    /// Defaults to "./cache".
+    /// </summary>
     public string CachePath { get; set; } = "./cache";
     
+    /// <summary>
+    /// Gets or sets the maximum number of retry attempts when a download fails.
+    /// Must be between 0 and 100. Defaults to 3.
+    /// </summary>
     [Range(0, 100)]
     public int MaxRetries { get; set; } = 3;
     
+    /// <summary>
+    /// Gets or sets the timeout in seconds for individual download operations.
+    /// Must be between 1 and 3600. Defaults to 300 (5 minutes).
+    /// </summary>
     [Range(1, 3600)]
     public int TimeoutSeconds { get; set; } = 300;
     
+    /// <summary>
+    /// Gets or sets a value indicating whether caching is enabled for downloaded coubs.
+    /// When true, previously downloaded coubs will be reused instead of re-downloading.
+    /// Defaults to true.
+    /// </summary>
     public bool EnableCaching { get; set; } = true;
     
+    /// <summary>
+    /// Gets or sets the maximum cache size in gigabytes before old entries are automatically removed.
+    /// Must be between 0.1 and 100.0. Defaults to 1.0 GB.
+    /// </summary>
     [Range(0.1, 100.0)]
     public double MaxCacheSizeGb { get; set; } = 1.0;
     
+    /// <summary>
+    /// Gets or sets the number of parallel download operations that can run simultaneously.
+    /// Must be between 1 and 50. Defaults to 4.
+    /// </summary>
     [Range(1, 50)]
     public int ParallelDownloads { get; set; } = 4;
 }

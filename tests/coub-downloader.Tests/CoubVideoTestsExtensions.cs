14d96c10b2a610627cce2ee9134698ae8698f444
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-11 11:51:29 +0000

    refactor(coub-downloader.tests): tighten guard clauses and docs in CoubVideoTestsExtensions

--- tests/coub-downloader.Tests/CoubVideoTestsExtensions.cs
+++ tests/coub-downloader.Tests/CoubVideoTestsExtensions.cs
@@ -16,6 +16,8 @@ public static class CoubVideoTestsExtensions
     /// <summary>
     /// Creates a video with portrait orientation (vertical format)
     /// </summary>
+    /// <param name="_">Test fixture parameter</param>
+    /// <returns>A new <see cref="CoubVideo"/> instance with vertical orientation</returns>
     public static CoubVideo CreateVerticalVideo(this CoubVideoTests _) => new()
     {
         Id = "vertical123",
@@ -29,6 +31,8 @@ public static class CoubVideoTestsExtensions
     /// <summary>
     /// Creates a video with landscape orientation and HD quality
     /// </summary>
+    /// <param name="_">Test fixture parameter</param>
+    /// <returns>A new <see cref="CoubVideo"/> instance with HD landscape orientation</returns>
     public static CoubVideo CreateHdLandscapeVideo(this CoubVideoTests _) => new()
     {
         Id = "hd123",
@@ -43,6 +47,8 @@ public static class CoubVideoTestsExtensions
     /// <summary>
     /// Creates a video with 4K quality dimensions
     /// </summary>
+    /// <param name="_">Test fixture parameter</param>
+    /// <returns>A new <see cref="CoubVideo"/> instance with 4K resolution</returns>
     public static CoubVideo Create4kVideo(this CoubVideoTests _) => new()
     {
         Id = "4k123",
@@ -57,6 +63,8 @@ public static class CoubVideoTestsExtensions
     /// <summary>
     /// Creates a video with a short duration (under 5 seconds)
     /// </summary>
+    /// <param name="_">Test fixture parameter</param>
+    /// <returns>A new <see cref="CoubVideo"/> instance with short duration</returns>
     public static CoubVideo CreateShortDurationVideo(this CoubVideoTests _) => new()
     {
         Id = "short123",
@@ -73,8 +81,11 @@ public static class CoubVideoTestsExtensions
     /// <param name="video">The video to check</param>
     /// <param name="threshold">Minimum view count to be considered popular</param>
     /// <returns>True if video has at least the threshold views, false otherwise</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="video"/> is null</exception>
     public static bool IsPopular(this CoubVideo video, long threshold = 10000)
     {
+        ArgumentNullException.ThrowIfNull(video);
+
         return video.ViewCount >= threshold;
     }
 
@@ -83,24 +94,18 @@ public static class CoubVideoTestsExtensions
     /// </summary>
     /// <param name="video">The video to categorize</param>
     /// <returns>A string describing the resolution category</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="video"/> is null</exception>
     public static string GetResolutionCategory(this CoubVideo video)
     {
-        if (video.Is4kQuality())
-        {
-            return "4K";
-        }
+        ArgumentNullException.ThrowIfNull(video);
 
-        if (video.IsHdQuality())
+        return video switch
         {
-            return "HD";
-        }
-
-        if (video.Width >= 1280 && video.Height >= 720)
-        {
-            return "SD";
-        }
-
-        return "Low";
+            _ when video.Is4kQuality() => "4K",
+            _ when video.IsHdQuality() => "HD",
+            _ when video.Width >= 1280 && video.Height >= 720 => "SD",
+            _ => "Low"
+        };
     }
 
     /// <summary>
@@ -108,16 +113,14 @@ public static class CoubVideoTestsExtensions
     /// </summary>
     /// <param name="video">The video with optional audio track</param>
     /// <returns>The total duration after accounting for audio looping</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="video"/> is null</exception>
     public static double GetTotalDurationWithAudio(this CoubVideo video)
     {
-        double baseDuration = video.Duration;
+        ArgumentNullException.ThrowIfNull(video);
 
-        if (video.AudioTrack != null)
-        {
-            baseDuration = video.CalculateRequiredAudioDuration();
-        }
-
-        return baseDuration;
+        return video.AudioTrack is null
+            ? video.Duration
+            : video.CalculateRequiredAudioDuration();
     }
 
     /// <summary>
@@ -125,8 +128,11 @@ public static class CoubVideoTestsExtensions
     /// </summary>
     /// <param name="video">The video to validate</param>
     /// <returns>True if video is valid and has minimum quality, false otherwise</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="video"/> is null</exception>
     public static bool IsProcessable(this CoubVideo video)
     {
+        ArgumentNullException.ThrowIfNull(video);
+
         return video.IsValid() && video.Duration > 0 && video.Width > 0 && video.Height > 0;
     }
 }
